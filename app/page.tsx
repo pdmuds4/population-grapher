92cae7939078b696c76627a2522468b5c27f22ec
pdmuds4 <ryosuke3191@gmail.com>
2024-06-16 23:12:42 +0900

    use server

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+// "use client"
 import { createContext, useState } from 'react';
 import { Header, MainBody, BoxList, Chart } from '@components';
 import { contextConfig, contextData } from 'context';
--- lib/components/Chart/index.tsx
+++ lib/components/Chart/index.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import "./style.css";
 import { useContext, useEffect, useState } from "react";
 import Selector from "./Selector";
